ba1946c4febaa784283345dcac3de95150dcec13
mhkane <mohamedkane@mohameds-mbp.attlocal.net>
2019-01-02 22:41:40 -0600

    p2E2

--- practical2.py
+++ practical2.py
@@ -8,3 +8,19 @@ def nearest_classification(train_input, train_target, x):
   index_shortest = int(torch.sort(torch.sum(dist_matrix,1))[1][0])
   label = int(train_target[index_shortest])
   return label
+
+
+#Exercise_2
+def compute_nb_errors(train_input, train_target, test_input, test_target, mean = None, proj = None):
+	error_count =0
+	if mean != None:
+		train_input = train_input-mean
+		test_input = test_input-mean
+  	if proj != None:
+    	train_input = torch.mm(train_input,torch.t(proj))
+    	test_input = torch.mm(test_input,torch.t(proj))
+  	for ind in range(len(test_input)):
+    	predicted = nearest_classification(train_input,train_target,test)
+    	if predicted != int(test_targert[ind]):
+      		error_count+=1
+	return error_count
