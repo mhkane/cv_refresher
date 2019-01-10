28cb56fbd9205e15a7ee3e313566f6f652f8eda9
mhkane <hassanmohamed0407@gmail.com>
2019-01-09 23:22:05 -0800

    Performance optimization

---
+++ perf_optimization.py
@@ -0,0 +1,15 @@
+import cv2
+e1 = cv2.getTickCount()
+# your code execution
+e2 = cv2.getTickCount()
+time = (e2 - e1)/ cv2.getTickFrequency()
+
+img1 = cv2.imread('messi.jpg')
+
+e1 = cv2.getTickCount()
+for i in xrange(5,49,2):
+    img1 = cv2.medianBlur(img1,i)
+e2 = cv2.getTickCount()
+t = (e2 - e1)/cv2.getTickFrequency()
+print t
+
