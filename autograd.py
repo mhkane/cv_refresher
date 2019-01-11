085ba48d327f30b318dfe0f0a5777fc81fa6597e
mhkane <hassanmohamed0407@gmail.com>
2019-01-10 18:51:29 -0800

    Autograd 2

--- autograd.py
+++ autograd.py
@@ -19,3 +19,13 @@ x = torch.tensor([1., 10.])
 x.requires_grad = True
 
 x = torch.tensor([1, 10])
+
+w1 = torch.rand(20, 10).requires_grad_()
+b1 = torch.rand(20).requires_grad_()
+w2 = torch.rand(5, 20).requires_grad_()
+b2 = torch.rand(5).requires_grad_()
+x = torch.rand(10)
+h = torch.tanh(w1 @ x + b1)
+y = torch.tanh(w2 @ h + b2)
+target = torch.rand(5)
+loss = (y - target).pow(2).mean()
