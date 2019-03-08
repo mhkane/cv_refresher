2904b012bbaf56b566ffc27f08ad16a39b820b5f
mhkane <hassanmohamed0407@gmail.com>
2019-03-08 12:25:55 +0100

    Add post training visualization

--- transfer_learning.py
+++ transfer_learning.py
@@ -177,3 +177,7 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                        num_epochs=25)
+
+#Post Training
+visualize_model(model_ft)
+
