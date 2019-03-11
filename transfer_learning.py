ef65378cdb1d8b25cfd8b4e0e2f235036d9e1285
mhkane <hassanmohamed0407@gmail.com>
2019-03-11 19:53:48 +0100

    Convnet as feature extractor

--- transfer_learning.py
+++ transfer_learning.py
@@ -181,3 +181,27 @@ model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
 #Post Training
 visualize_model(model_ft)
 
+
+
+model_conv = torchvision.models.resnet18(pretrained=True)
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+# Parameters of newly constructed modules have requires_grad=True by default
+num_ftrs = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_ftrs, 2)
+
+model_conv = model_conv.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that only parameters of final layer are being optimized as
+# opposed to before.
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+model_conv = train_model(model_conv, criterion, optimizer_conv,
+                         exp_lr_scheduler, num_epochs=25)
+
