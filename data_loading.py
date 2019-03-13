08e7b68a3fb66e9006d50d6304e7d1c9d1c3e2ec
mhkane <hassanmohamed0407@gmail.com>
2019-03-13 19:59:20 +0100

    Iterator

--- data_loading.py
+++ data_loading.py
@@ -67,3 +67,23 @@ class FaceLandmarksDataset(Dataset):
             sample = self.transform(sample)
 
         return sample
+
+face_dataset = FaceLandmarksDataset(csv_file='data/faces/face_landmarks.csv',
+                                    root_dir='data/faces/')
+
+fig = plt.figure()
+
+for i in range(len(face_dataset)):
+    sample = face_dataset[i]
+
+    print(i, sample['image'].shape, sample['landmarks'].shape)
+
+    ax = plt.subplot(1, 4, i + 1)
+    plt.tight_layout()
+    ax.set_title('Sample #{}'.format(i))
+    ax.axis('off')
+    show_landmarks(**sample)
+
+    if i == 3:
+        plt.show()
+        break
